d833d9bb08cc05c807c227212dc6144f2820035b
kedpinted <pinted.ked@gmail.com>
2016-03-08 05:39:32 +0700

    add

--- dashboard.php
+++ dashboard.php
@@ -4,21 +4,21 @@
     <meta charset="utf-8">
     <title>Annotation | Dashboard</title>
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="bower_components/mediaelement/build/mediaelementplayer.min.css">
+    
     <link rel="stylesheet" type="text/css" href="assets/css/video.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
 
     <script src="assets/vendor/jquery/jquery.min.js"></script>
     <script src="assets/vendor/jquery/jquery-ui.min.js"></script>
-
+    
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/node-waves/0.7.4/waves.min.js"></script>
     <script type="text/javascript" src="assets/vendor/bootstrap/bootstrap.min.js"></script>
     <script type="text/javascript" src="bower_components/mediaelement/build/mediaelement-and-player.min.js"></script>
     <script type="text/javascript" src="assets/vendor/jquery.signature/jquery.signature.min.js"></script>
-    <!--script type="text/javascript" src="assets/js/handwriting.js"></script-->
     <script type="text/javascript" src="assets/js/main.js"></script>
 </head>
 <body>
@@ -44,13 +44,50 @@
                     <div class="thumbnail_image">
                         <div class="hover_icon_container">
                             <div class="hover_icons">
-                                <a href="#" class="hover_icon"></a>
+                                <!--<a href="#" class="hover_icon"></a>-->
+                                <i class="fa fa-play-circle-o fa-4x"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="utility_bar">
+                        <div class="bottom_bar_container">
+                            <div class="bottom_bar">
+                                <div class="icon">
+                                    <i data-toggle="tooltip" title="Delete" class="fa fa-trash-o fa-lg"></i>
+                                </div>
+                                <div class="icon right">
+                                    <i class="fa fa-trash-o fa-lg"></i>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div class="cards_container">
+                <div class="content_card universal_card">
+                    <div class="thumbnail_image">
+                        <div class="hover_icon_container">
+                            <div class="hover_icons">
+                                <a href="video.php" class="hover_icon"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="utility_bar">
+                        <div class="bottom_bar_container">
+                            <div class="bottom_bar">
+                                <div class="icon">
+                                    <i class="fa fa-trash-o fa-lg"></i>
+                                </div>
+                                <div class="icon right">
+                                    <i class="fa fa-trash-o fa-lg"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!--flex_cards-->
+
        <!--<?php
             /*@session_start();
             include("connectdb.php");
